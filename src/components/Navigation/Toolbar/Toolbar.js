0ee2ff7eaf5fdb808a37e2fabb7c9ce7756f73d8
Cameron Matlock <cameron.matlock@ruralsource.com>
2021-02-24 15:14:52 -0600

    added navigation and order summary

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,16 @@
+import Logo from '../../Logo/Logo';
+import React from 'react';
+import classes from './Toolbar.module.css';
+import NavigationItems from '../NavigationItems/NavigationItems';
+
+const toolbar = (props) => (
+    <header className={classes.Toolbar}>
+        <div>MENU</div>
+        <Logo />
+        <nav>
+            <NavigationItems />
+        </nav>
+    </header>
+);
+
+export default toolbar;
